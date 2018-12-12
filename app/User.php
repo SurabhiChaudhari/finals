09468abfd684dad0242579caca972a0e1cb566e3
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-11 22:21:57 -0500

    Added user profile migrate, model, seed and updated user seederto create profiles.

--- app/User.php
+++ app/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function Profile()
+    {
+        return $this->hasOne('App\Profile');
+    }
 }
