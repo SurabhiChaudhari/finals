0d8dd0fb608dd82aa8561cf5c71a2e9da1e91356
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-11 21:53:22 -0500

    User Created userTableSeeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('email')->unique();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
