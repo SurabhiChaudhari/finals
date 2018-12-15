ea70583242e5703dd7fc08217faf93b8f61f4c31
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-15 18:09:55 -0500

    Task: Added UserLoginDuskTest and test result passed

--- tests/Browser/RegistrationUserDuskTest.php
+++ tests/Browser/RegistrationUserDuskTest.php
@@ -17,7 +17,7 @@ class RegisterUserTest extends DuskTestCase
                 ->assertTitle('Laravel')
                 ->clickLink('Register')
                 ->assertSee('Register')
-                ->type('#email', 'suru200@gmail.com')
+                ->type('#email', 'suru20000@gmail.com')
                 ->type('#password', 'Welcome1')
                 ->type('#password-confirm', 'Welcome1')
                 ->click('button[type="submit"]');
