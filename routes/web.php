4d3c128d8042066dfa440466fd3ccd6e1b79e8d7
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-15 07:45:30 -0500

    Task : Added the edit and update profile functions to edit the profile and save it.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/question/{question_id}/answer/{answer_id}', 'AnswerController@show'
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::post('/user/{user_id}/profile/', 'ProfileController@store')->name('profile.store');
+Route::get('/user/{user_id}/profile/{profile_id}/edit', 'ProfileController@edit')->name('profile.edit');
+Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
+
 Route::resources([
     'question' => 'QuestionController',
 ]);
