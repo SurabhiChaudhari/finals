58a1cc73aba86cfcf549b3b3c52ed4daba8a1ba8
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-15 09:33:22 -0500

    Task: Installed Dusk and Browser folder and DuskTestCase got created

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.16 on 2018-12-14 20:24:05.
+ * Generated for Laravel 5.7.16 on 2018-12-15 14:01:09.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
