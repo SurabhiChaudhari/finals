7bba068cbf14ac01f7a08158b6e07c20ba827ba7
Surabhi Chaudhari <sc2263@njit.edu>
2018-12-15 18:01:28 -0500

    Task: Added RegistrationUserDuskTest and test result passed

---
+++ tests/Browser/RegistrationUserDuskTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests\Browser;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+class RegisterUserTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testRegisterUser()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('http://localhost:8000')
+                ->assertTitle('Laravel')
+                ->clickLink('Register')
+                ->assertSee('Register')
+                ->type('#email', 'suru200@gmail.com')
+                ->type('#password', 'Welcome1')
+                ->type('#password-confirm', 'Welcome1')
+                ->click('button[type="submit"]');
+        });
+    }
+}
